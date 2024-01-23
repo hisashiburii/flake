6f0911f3e1dbddfb87bd8370cbb9b6f27c8b7abe
hisashiburi <tophersurp@gmail.com>
2024-01-23 02:16:25 -0800

    test

--- configuration.nix
+++ configuration.nix
@@ -10,6 +10,7 @@
     # Include the results of the hardware scan.
     ./hardware-configuration.nix
     ./comma.nix
+    ./home.nix
   ];
 
   # Bootloader.
---
+++ home.nix
@@ -0,0 +1,11 @@
+{pkgs, ...}: {
+    programs.kitty {
+        settings = {
+            confirm_os_window_close = 0;
+        };
+
+        enable = true;
+
+        theme = "Catppuccin-Mocha";
+    };
+}
