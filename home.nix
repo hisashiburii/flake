c5bf07e6c5ebb0561e1aba5e0a965c838a43b0a9
hisashiburi <tophersurp@gmail.com>
2024-01-23 02:57:54 -0800

    i hate getchoo im gonna murder them this is my confession

--- home.nix
+++ home.nix
@@ -1,4 +1,7 @@
 {pkgs, ...}: {
+
+    home.stateVersion = "24.05";
+
     programs.kitty = {
         settings = {
             confirm_os_window_close = 0;
@@ -8,4 +11,30 @@
 
         theme = "Catppuccin-Mocha";
     };
+
+    programs.neovim = {
+        enable = true;
+    };
+
+    programs.dunst = {
+        enable = true;
+    };
+
+    programs.git.enable = true;
+
+    programs.gh.enable = true;
+
+    programs.wlogout.enable = true;
+
+    programs.feh.enable = true;
+
+    programs.htop.enable = true;
+
+    programs.obs-studio.enable = true;
+
+    programs.bat.enable = true;
+
+    programs.vscode.enable = true;
+    programs.vscode.package = pkgs.vscode.fhs;
+
 }
