a80a229a6aa5c0927a55371e8358353357707fb5
hisashiburi <tophersurp@gmail.com>
2024-01-27 08:20:32 -0800

    add catppuccinifer flake input

--- flake.nix
+++ flake.nix
@@ -12,6 +12,10 @@
       url = "github:nix-community/nix-index-database";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+      catppuccinifier = {
+      url = "github:lighttigerXIV/catppuccinifier";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs = {nixpkgs, ...} @ inputs: {
